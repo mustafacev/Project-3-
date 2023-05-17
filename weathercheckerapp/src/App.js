aaeb7baf07c44654fdc35e477405f880ef6b6a94
mustafacev <mustafaorcuncevik@gmail.com>
2023-05-16 20:05:39 -0400

    created weatherboard

--- weathercheckerapp/src/App.js
+++ weathercheckerapp/src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 
 function App() {
   return (
-
-  }
+    <div className="App">
+      {/* dt is in unix-seconds but javascript uses milliseconds, multiply with 1000 */}
+      <WeatherBoard
+        dt={1602104400 * 1000}
+        temp_min="22.67"
+        temp_max="24.39"
+        main="Clear"
+        icon="01d" />
+    </div>
+  );
+}
 
 export default App;
