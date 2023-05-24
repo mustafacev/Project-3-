bcfbd0c30b043eb76f81f129fe4358c2878c20dc
mustafacev <mustafaorcuncevik@gmail.com>
2023-05-23 22:16:11 -0400

    updated CityBoard icon issue

--- weathercheckerapp/src/components/CityBoard.js
+++ weathercheckerapp/src/components/CityBoard.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-const CityBoard = (props) => {
+import "./CityBoard.css";
+
+const CityBoard = (props)=>{
   const { getCityData, fetchData } = props;
 
   return (
-    <div>
-      <img src="./icon/perfect-day.svg" alt="weathergood" />
-      <br></br>
-      <span className="select-city">Check Your City</span>
+      <div>
+      <img src="http://openweathermap.org/img/w/01d.png" alt="weathergood"/>
+      <span className="select-city">Find Weather of your city</span>
       <form onSubmit={fetchData}>
-        <input onChange={(e) => getCityData(e.target.value)}
+          <input onChange={(e) => getCityData(e.target.value)}
           placeholder="City" />
-        <button type="submit">Search</button>
+          <button type="submit">Search</button>
       </form>
-    </div>
+      </div>
   );
 };
 export default CityBoard;
