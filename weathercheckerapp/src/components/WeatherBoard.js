ffc4292422aeb6d9862088414b663d707651fcab
mustafacev <mustafaorcuncevik@gmail.com>
2023-05-23 21:58:00 -0400

    updating the WeatherBoard

--- weathercheckerapp/src/components/WeatherBoard.js
+++ weathercheckerapp/src/components/WeatherBoard.js
@@ -1,55 +1,54 @@
 import React from 'react';
-import icons from './icons';
+import "./WeatherBoard.css";
+import { IconContext } from 'react-icons';
+import { FaWind  } from "react-icons/fa";
+import { WiHumidity  } from "react-icons/wi";
 
-export const WeatherInfoIcons = {
-  sunset: "./icon/temp.svg",
-  sunrise: "./icon/temp.svg",
-  humidity: "/icon/humidity.svg",
-  wind: "./icon/wind.svg",
-  pressure: "./icon/pressure.svg",
-};
-
-const WeatherBoard = (props) => {
-  const { name, value } = props;
-  return (
-    <div className="InfoContainer">
-      <img src={WeatherInfoIcons[name]} alt="weathericon" />
-      <span className="InfoLabel">
-        {value}
-        <span>{name}</span>
-      </span>
-    </div>
+const WeatherBoard =(props)=>{
+  const{ name, value } = props;
+  return(
+      <div className="InfoContainer">
+          <span className="InfoLabel">
+              {value}
+              <span>{name}</span>
+          </span>
+      </div>
   );
 };
 
 const WeatherDetails = (props) => {
-  const { weather } = props;
+  const {weather}=props;
   const checkDay = weather?.weather[0].icon?.includes('d');
+  const iconCode = weather?.weather[0].icon;
+  const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
   const getTime = (timeStamp) => {
-    return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
+      return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
   };
-  return (
-    <div>
-      <div className="weather-condition">
-        <span className="details">
+return (
+  <div>
+    <div className="weather-condition">
+      <span className="details">
           <span>{`${Math.floor(weather?.main?.temp - 273)}°C`}</span>
           {`  |  ${weather?.weather[0].description}`}
-        </span>
-        <img src={icons[weather?.weather[0].icon]} alt="weather-d-icon" />
-      </div>
-
-      <span className="city">{`${weather?.name}, ${weather?.sys?.country}`}</span>
-
-      <span className="WeatherInfoLabel">Weather Info</span>
-      <div className="WeatherInfoContainer">
-        <WeatherBoard
-          name={checkDay ? "sunset" : "sunrise"}
-          value={`${getTime(weather?.sys[checkDay ? "sunset" : "sunrise"])}`}
-        />
-        <WeatherBoard name="humidity" value={weather?.main?.humidity} />
-        <WeatherBoard name="wind" value={weather?.wind?.speed} />
-      </div>
+          </span>
+          <img src={iconUrl} alt="weather-icon"/>
     </div>
-  );
+
+    <span className="city">{`${weather?.name}, ${weather?.sys?.country}`}</span>
+    <br></br>
+    <span className="WeatherInfoLabel"> Weather Info</span>
+    <div className="WeatherInfoContainer">
+      <WeatherBoard
+       name={checkDay ? " sunset" : " sunrise"}
+      value={`${getTime(weather?.sys[checkDay ? "sunset" : "sunrise"])}`}
+       />
+      <WiHumidity/> 
+      <WeatherBoard name=" humidity" value={weather?.main?.humidity} />
+      <FaWind/>
+      <WeatherBoard name=" wind" value={weather?.wind?.speed} />
+    </div>
+  </div>
+);
 };
 export default WeatherDetails;
