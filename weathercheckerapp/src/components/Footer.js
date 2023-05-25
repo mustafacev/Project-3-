2d70bc8009c65492409ca6b28548077bdf2e3630
mustafacev <mustafaorcuncevik@gmail.com>
2023-05-24 20:08:01 -0400

    updating the WeatherBoard

--- weathercheckerapp/src/components/Footer.js
+++ weathercheckerapp/src/components/Footer.js
@@ -4,6 +4,8 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
+        <br></br>
+        <br></br>  
       <p className="footer-text">© {new Date().getFullYear()} Weather Checker App by Mustafa. All rights reserved.</p>
     </footer>
   );
--- weathercheckerapp/src/components/Header.js
+++ weathercheckerapp/src/components/Header.js
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Header.css';
 
-function Header() {
-    return (
-        <>
-
-            <h1>WeatherChecker App</h1>
-        </>
-    )
-}
+const Header = () => {
+  return (
+    <header className="header">
+      <h3 className="header-title">Weather Checker App</h3>
+    </header>
+  );
+};
 
 export default Header;
